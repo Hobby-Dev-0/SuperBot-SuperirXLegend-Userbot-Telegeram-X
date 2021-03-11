77dec1a888d2d7f70b853cbff2a4620fc9b2c802
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-03-11 09:03:59 +0530

    Update awake.py

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -1,10 +1,10 @@
 """
-(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
-(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
-(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
-(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
-MADE BY @LEGENDX22 dont kang this plugin
-CREDITS = @LEGENDX22 @PROBOYX @alain_champion
+(((((((((((((((((((((((@LEGENDX22, @PythonProgramingIN)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22, @PythonProgramingIN)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22, @PythonProgramingIN)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22, @PythonProgramingIN)))))))))))))))))))))))))))
+MADE BY @LEGENDX22, @PythonProgramingIN dont kang this plugin
+CREDITS = @LEGENDX22 @PROBOYX @alain_champion, @PythonProgramingIN
 Special thanks @alain_champion for this modified version
 if you kang then keep credits
 """
@@ -28,7 +28,7 @@ from platform import python_version, uname
 
 ALIVE_PHOTTO = Config.ALIVE_PHOTTO
 if ALIVE_PHOTTO is None:
-  ALIVE_PHOTTO = "https://telegra.ph/file/0e36b02061064b7229e3b.jpg"
+  ALIVE_PHOTTO = "https://telegra.ph/file/fd0c6732fa1fdfeae2419.png"
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND BOY"
@@ -41,15 +41,14 @@ global ghanti
 async def amireallyalive(awake):
    """ For .awake command, check if the bot is running.  """
    tag = borg.uid
-   uptm = await legend.get_readable_time((time.time() - StartTime))
+   uptm = await Superior.get_readable_time((time.time() - StartTime))
    ALIVE_MESSAGE= f" ⚡️ {BOT} ⚡️  IS ON 🔥 FIRE 🔥"
    ALIVE_MESSAGE += "\n\n"
    ALIVE_MESSAGE += "💟 𝚂𝚈𝚂𝚃𝙴𝙼 𝚂𝚃𝙰𝚃𝚄𝚂 💟\n\n"
    ALIVE_MESSAGE += "☎️ 𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ☎️ : 6.0.9\n\n"
-   ALIVE_MESSAGE += "🔶 𝙻𝙴𝙶𝙴𝙽𝙳 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 🔶 :   2.8\n\n"
+   ALIVE_MESSAGE += "🔶 Superioir  𝚅𝙴𝚁𝚂𝙸𝙾𝙽 🔶 :   2.8\n\n"
    ALIVE_MESSAGE += f"🔷 𝚄𝙿𝚃𝙸𝙼𝙴 🔷 : {uptm}\n\n"
    ALIVE_MESSAGE += f"💠 𝙼𝚈 𝙱𝙾𝚂𝚂 💠: [{DEFAULTUSER}](tg://user?id={tag})\n\n"
-   ALIVE_MESSAGE += "🔰 𝙶𝚁𝙾𝚄𝙿 🔰 : [SUPPORT](https://t.me/LEGEND_USERBOT_SUPPORT)\n\n"
    ALIVE_MESSAGE += f"💠 [𝙳𝙴𝙿𝙻𝙾𝚈](https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FLEGENDXOP%2FLEGEN-BOT&template=https%3A%2F%2Fgithub.com%2FLEGENDXOP%2FLEGEND-ROBOT) 𝚈𝙾𝚄𝚁 𝙾𝚆𝙽 𝙾𝙿 [{BOT}](http://github.com/legendxop/legend-bot)  💠\n"   
    await awake.delete() 
    await borg.send_file(awake.chat_id, ALIVE_PHOTTO,caption=ALIVE_MESSAGE)
